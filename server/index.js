8e123b14f6eab22a3146b35d46266aac46a568fd
Logan <ryanx.logan@intel.com>
2021-12-29 16:49:02 -0800

    completed user routes for backend, auth, models

--- server/index.js
+++ server/index.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
-app.use(cors());
-app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
- 
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
-  console.log(`Server is running on port: ${port}`);
-});
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
 
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("connected to DB"))
+  .catch((err) => console.log(err));
+
+  app.use(express.json())
+
+  app.use("/api/user", userRoute);
+  app.use("/api/auth", authRoute);
+
+app.listen(process.env.PORT || 5000, () => console.log("server running"));
