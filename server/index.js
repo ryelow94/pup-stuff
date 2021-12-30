3fd5d68aaa59bfb1c244fa044c42a0caa52e6b42
Logan <ryanx.logan@intel.com>
2021-12-30 11:29:48 -0800

    added stripe, and more backend, completed CRUD

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ dotenv.config();
 const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
 const productRoute = require("./routes/product")
+const cartRoute = require("./routes/cart")
+const orderRoute = require("./routes/order")
+const stripeRoute = require("./routes/stripe")
+const cors =  require("cors");
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -14,8 +18,13 @@ mongoose
 
   app.use(express.json())
 
+  app.use(cors())
+
   app.use("/api/users", userRoute);
   app.use("/api/auth", authRoute);
   app.use("/api/products", productRoute);
+  app.use("/api/carts", cartRoute);
+  app.use("/api/orders", orderRoute);
+  app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 4000, () => console.log("server running"));
