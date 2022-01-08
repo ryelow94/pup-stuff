2267c85e729be19cd569980b965e1976af1c1a98
Logan <ryanx.logan@intel.com>
2022-01-07 16:18:48 -0800

    started validation for sign up form, still need to validate email correctly

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 import { useState } from "react";
-import { useDispatch } from "react-redux"
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
-import { signUp } from "../redux/apiCalls"
-
+import { signUp } from "../redux/apiCalls";
 
 const Container = styled.div`
   width: 100vw;
@@ -58,18 +57,55 @@ const Button = styled.button`
     background-color: #f8f4f4;
   }
 `;
+const Match = styled.span`
+  text-align: center;
+  color: red;
+  font-size: 12px;
+  margin-top: 20px;
+`;
 
 export const SignUp = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const dispatch = useDispatch()
-  const { isFetching, error} = useSelector((state) => state.user)
+  const [confirm, setConfirm] = useState("");
+  const [match, setMatch] = useState(true);
+  const [validEmail, setValidEmail] = useState(true);
+  const [required, setRequired] = useState(false);
+  const dispatch = useDispatch();
+  const { isFetching, error } = useSelector((state) => state.user);
+
+  const validate = () => {
+    if (
+      firstName === "" ||
+      lastName === "" ||
+      email === "" ||
+      password === ""
+    ) {
+      setRequired(true);
+    }
+    if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)){
+      setValidEmail(false)
+    } else {
+      setValidEmail(true)
+    }
+    if (password !== confirm) {
+      setMatch(false);
+    }
+  };
 
   const handleClick = (e) => {
     e.preventDefault();
-   signUp(dispatch,  {email, password, firstName,lastName})
+    validate();
+    console.log(validEmail)
+    if (
+      required===false &&
+      password===confirm &&
+      validEmail===true
+    ) {
+      signUp(dispatch, { email, password, firstName, lastName });
+    }
   };
   return (
     <Container>
@@ -85,22 +121,33 @@ export const SignUp = () => {
             onChange={(e) => setLastName(e.target.value)}
           />
           <Input
+            type="email"
             placeholder="Email"
             onChange={(e) => setEmail(e.target.value)}
           />
           <Input
             placeholder="Password"
+            type="password"
             onChange={(e) => setPassword(e.target.value)}
           />
-          <Input placeholder="Confirm Password" />
+          <Input
+            placeholder="Confirm Password"
+            type="password"
+            onChange={(e) => setConfirm(e.target.value)}
+          />
           <Agreement>
             By creating an account, I consent to the use of my personal
             information in accordance with our <b>Privacy Policy</b>
           </Agreement>
-          <Button onClick={handleClick} disabled={isFetching} >
+          <Button onClick={handleClick} disabled={isFetching}>
             Sign Up
           </Button>
         </Form>
+        {match===false ? <Match>Passwords do not match</Match> : null}
+        <br></br>
+        {validEmail===false ? <Match>Invalid email address</Match> : null}
+        <br></br>
+        {required === true ? <Match>All fields must be entered</Match> : null}
       </Wrapper>
     </Container>
   );
